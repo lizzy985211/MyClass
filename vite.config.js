ae28d73c3e8523b584177134a7b1fdfc8d7c7ad8
tbb <franktang196@gmail.com>
2025-09-01 17:11:03 +0800

    调整部署路径

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
 	plugins: [vue()],
+	base: '/MyClass/',  // 👈 注意这里要和仓库名一致
 	server: { port: 5173, open: false },
 	preview: { port: 5173 }
 })
