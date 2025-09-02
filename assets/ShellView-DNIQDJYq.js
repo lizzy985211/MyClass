f2af3d14a1fc93541a3442be061e16240dc8064a
lizzy985211 <lizzy985211@users.noreply.github.com>
2025-09-02 09:28:27 +0000

    deploy: 7ec7f200d4150cf9b37a41df2569eae220bc2a02

---
+++ assets/ShellView-DNIQDJYq.js
@@ -0,0 +1,1 @@
+import{_ as o,c as r,a as n,b as s,r as a,o as c}from"./index-DXsCtUvq.js";const i={},_={class:"container",style:{"margin-top":"12px"}};function l(p,e){const t=a("router-view");return c(),r("div",_,[e[0]||(e[0]=n("h2",{style:{"text-align":"center",margin:"8px 0 16px"}},"武汉市小学成绩统计与分析",-1)),s(t)])}const d=o(i,[["render",l]]);export{d as default};
